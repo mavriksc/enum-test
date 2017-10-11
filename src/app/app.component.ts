da0da2b15a01b9ada229d5714383adb41d718491
Sean Carlisle <scarlisle@hylamobile.com>
2017-10-11 11:30:58 -0500

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 enum States  {
   STATE_ONE,STATE_TWO,STATE_THREE
 }
@@ -16,42 +19,83 @@ class IHaveStates {
 })
 export class AppComponent {
   title = 'app works!';
+  sublist: States[]=[States.STATE_TWO, States.STATE_THREE];
+  canICancel:boolean = false;
 
+  constructor(private http: Http) {}
 
-  ngOnInit(){
-    const sublist: States[]=[States.STATE_TWO, States.STATE_THREE ];
+  getState(): Promise<IHaveStates> {
+    return this.http.get('http://localhost:8080')
+      .toPromise()
+      .then(response => response.json() as IHaveStates)
+      .catch();
+  }
+
+  stateTest(){
     let thing: IHaveStates = new IHaveStates();
 
     thing.state = States.STATE_THREE;
-    let index = sublist.indexOf(thing.state);
+    let index = this.sublist.indexOf(thing.state);
     let statesICanCancel = index ===-1;
+    console.log('State of the thing: '+thing.state);
+    console.log('Enum value of State of the thing: '+States[thing.state]);     
+    console.log('Sub list of can\'t cancel states: '+this.sublist);
     console.log('location cant cancel list: '+index);
-
     console.log('can i cancel: '+statesICanCancel);
-    console.log(thing.state);
-    console.log(States[thing.state]);
-    console.log(States[States[thing.state]]);
 
     thing.state = States.STATE_ONE;
-    index = sublist.indexOf(thing.state);
+    index = this.sublist.indexOf(thing.state);
     statesICanCancel = index ===-1;
+    console.log('State of the thing: '+thing.state);
+    console.log('Enum value of State of the thing: '+States[thing.state]);     
+    console.log('Sub list of can\'t cancel states: '+this.sublist);
     console.log('location cant cancel list: '+index);
-
     console.log('can i cancel: '+statesICanCancel);
-    console.log(thing.state);
-    console.log(States[thing.state]);
-    console.log(States[States[thing.state]]);
 
     thing.state = States['STATE_ONE'];
-    index = sublist.indexOf(thing.state);
+    index = this.sublist.indexOf(thing.state);
     statesICanCancel = index ===-1;
+    console.log('State of the thing: '+thing.state);
+    console.log('Enum value of State of the thing: '+States[thing.state]);     
+    console.log('Sub list of can\'t cancel states: '+this.sublist);
     console.log('location cant cancel list: '+index);
-
     console.log('can i cancel: '+statesICanCancel);
-    console.log(thing.state);
-    console.log(States[thing.state]);
-    console.log(States[States[thing.state]]);
 
+    thing = {state:0};//error trying to set with state:'STATE_ONE'
+    index = this.sublist.indexOf(thing.state);
+    statesICanCancel = index ===-1;
+    console.log('State of the thing: '+thing.state);
+    console.log('Enum value of State of the thing: '+States[thing.state]);     
+    console.log('Sub list of can\'t cancel states: '+this.sublist);
+    console.log('location cant cancel list: '+index);
+    console.log('can i cancel: '+statesICanCancel);
 
   }
+  canCancel(thing:IHaveStates):boolean{
+    for (let state of this.sublist) {
+      if (thing.state.toString() === States[state]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  ngOnInit(){
+    this.stateTest();
+    this.getState().then(thing => { this.canICancel = this.canCancel(thing);
+      this.title = thing.state +'<---- state  can cancel---->'+ this.canICancel;
+
+      console.log('State of the thing: '+thing.state);
+      console.log('Enum value of State of the thing: '+States[thing.state]);     
+      console.log('Sub list of can\'t cancel states: '+this.sublist);
+
+      console.log('New method can cancel: '+this.canICancel);
+
+
+    });
+
+  }
+
+
+  
 }
